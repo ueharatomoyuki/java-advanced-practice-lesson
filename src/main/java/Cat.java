4a4e48b5f7d5cbb5d61e5bfd0bc5cfa0c45c355f
uehara tomoyuki <22uehara_tomoyuki@axiz.co.jp>
2022-05-11 18:21:58 +0900

    totyu

---
+++ src/main/java/Cat.java
@@ -0,0 +1,19 @@
+
+
+public class Cat extends Animal {
+	public Cat () {
+		
+	}
+	
+	public Cat(String a ,int b) {
+	
+	}
+	
+	public String intoroduceAge() {
+		return
+	}
+	
+	public String getFilePath() {
+		return
+	}
+}
---
+++ src/main/java/Rabbit.java
@@ -0,0 +1,19 @@
+
+
+public class Rabbit extends Animal{
+	public Rabbit() {
+		
+	}
+	
+	public Rabbit(String a, int b) {
+		
+	}
+	
+	public String introduceAge() {
+		return "種類はウサギです。人間でいうと　です" ; 
+	}
+	
+	public String getFilePath() {
+		return
+	}
+}
